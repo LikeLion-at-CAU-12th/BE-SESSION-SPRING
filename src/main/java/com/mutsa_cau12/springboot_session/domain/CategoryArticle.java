00c4dbe971b2f9470847a1255c012f21b52e4713
hayeongKo <polarpheno@icloud.com>
2024-09-10 02:03:58 +0900

    [FEAT] 엔티티 생성

---
+++ src/main/java/com/mutsa_cau12/springboot_session/domain/CategoryArticle.java
@@ -0,0 +1,25 @@
+package com.mutsa_cau12.springboot_session.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CategoryArticle {
+    @Id @GeneratedValue
+    @Column(name = "category_article_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
